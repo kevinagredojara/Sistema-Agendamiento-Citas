493d8426c1e3fb34479b5fb206b8fc0d6502d3e3
Kevin Agredo <kevinagredojara@outlook.com>
2025-06-05 10:39:44 -0500

    Version estable de despliegue en Azure

--- core_project/settings.py
+++ core_project/settings.py
@@ -213,7 +213,7 @@ if 'test' in sys.argv or os.environ.get('TESTING'):
 
 # ========== CONFIGURACIONES PARA AZURE ==========
 if os.getenv('AZURE_DEPLOYMENT'):
-    DEBUG = True # ¡MUY IMPORTANTE para producción!
+    DEBUG = False # ¡MUY IMPORTANTE para producción!
     
     # Asegúrate de que este es tu dominio real de Azure App Service
     ALLOWED_HOSTS = [
