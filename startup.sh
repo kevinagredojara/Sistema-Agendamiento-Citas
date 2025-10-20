3968b54e9728b0c94bf6113f2692aca0145d82cb
Kevin Agredo <kevinagredojara@outlook.com>
2025-10-20 14:09:06 -0500

    feat: Añadir comando temporal para restaurar superusuario

--- startup.sh
+++ startup.sh
@@ -7,8 +7,13 @@ python manage.py collectstatic --no-input
 echo "Applying database migrations..."
 python manage.py migrate
 
+# La línea de creación original puede quedarse, no hará nada.
 echo "Creating initial superuser if it does not exist..."
 python manage.py create_initial_superuser
+
+# ----> AÑADE ESTA NUEVA LÍNEA <----
+echo "Restoring superuser permissions..."
+python manage.py restore_superuser
 
 echo "Starting Gunicorn server..."
 gunicorn core_project.wsgi
