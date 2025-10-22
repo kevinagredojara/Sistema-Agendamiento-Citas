900336c674e0fa1d5e6c6ae4bc14fd495462089f
Kevin Agredo <kevinagredojara@outlook.com>
2025-10-22 09:18:23 -0500

    Limpieza manage.py

--- manage.py
+++ manage.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""
+Utilidad de línea de comandos de Django para el Sistema de Agendamiento.
+Ejecuta comandos administrativos del proyecto.
+"""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Ejecuta tareas administrativas de Django."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_project.settings')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "No se pudo importar Django. Verifica que esté instalado y "
+            "que hayas activado el entorno virtual correctamente."
         ) from exc
+    
     execute_from_command_line(sys.argv)
 
 
