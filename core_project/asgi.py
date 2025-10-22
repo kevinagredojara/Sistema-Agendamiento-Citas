84c31752c047d7722b796bb065d6a2d92425580f
Kevin Agredo <kevinagredojara@outlook.com>
2025-10-22 09:31:38 -0500

    Limpieza finalizada fase 2

--- core_project/asgi.py
+++ core_project/asgi.py
@@ -1,14 +1,8 @@
 """
-ASGI config for core_project project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+Configuración ASGI para Sistema de Agendamiento de Citas.
+Expone la aplicación ASGI para servidores asíncronos (Daphne, Uvicorn).
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_project.settings')
--- core_project/wsgi.py
+++ core_project/wsgi.py
@@ -1,14 +1,8 @@
 """
-WSGI config for core_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+Configuración WSGI para Sistema de Agendamiento de Citas.
+Expone la aplicación WSGI para servidores web de producción (Gunicorn, uWSGI).
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core_project.settings')
